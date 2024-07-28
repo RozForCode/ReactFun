266aaa2f4b69d975f7b9e20da9c27fa1a8477968
Navrose <navrosesinghjohal@gmail.com>
2024-07-28 16:58:37 -0400

    useState and useEffect added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+//use Effect happens alongside render
+// if you use empty array effect only happens once
+// useEffect(() => {
+//   console.log(`It's ${emotion} right now`)
+// }, [])
+// or we can pass an property or state so that when it changes the effect occurs
+// useEffect(() => {
+//   console.log(`It's ${emotion} right now`)
+// }, [emotion])
+
+
+//example of destructoring, we can also do that for arrays {below}
+function App({ library }) {
+  const [emotion, setEmotion] = useState("Happy",);
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>Hello from {library}</p>
+        <p>Current emotion is {emotion}</p>
+        <button onClick={() => { setEmotion("sad") }}>Sad</button>
+        <button onClick={() => { setEmotion("excited") }}>Excited</button>
+
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +40,17 @@ function App() {
   );
 }
 
+
+
 export default App;
+
+
+//Notes - managing state in very imp, modern way to handle state variables in an app 
+// is to use a function called useState
+
+//example of array destructuring
+// const [first, second, third, fourth] = ['Mississauga', 'Brampton', 'Oakville', 'Vauhon']
+// console.log(first);
+
+//Note -useEffect - another important hook - that is used to manage side-effects that aren't related to
+// a rendered component - working with animations
