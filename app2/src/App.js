0facb93ba839817a2e33ad81debe7e22b0ed4c04
Navrose <navrosesinghjohal@gmail.com>
2024-07-28 23:35:51 -0400

    made use of useRef no pun intended

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useRef } from 'react'
+// ref- way to react out to individual elements and check it's value
+// good for handling uncontrolled components -- useRef is not going to reRender so we always have to reach out to get the value
 function App() {
+  const txtTitle = useRef(),
+    hexColor = useRef();
+  const submit = e => {
+
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={submit}>
+          <input ref={txtTitle} type="text" placeholder='color title..' />
+          <input ref={hexColor} type="color" />
+          <button>ADD</button>
+        </form>
       </header>
     </div>
   );
