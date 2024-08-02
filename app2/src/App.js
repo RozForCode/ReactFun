04e649cf6addd5619ef309c39efcdd5276b4fa32
Navrose <navrosesinghjohal@gmail.com>
2024-08-01 23:40:47 -0400

    added, useState to do the same function as useRef

--- app2/src/App.js
+++ app2/src/App.js
@@ -2,16 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 import { useRef } from 'react'
+import { useState } from 'react';
 // ref- way to react out to individual elements and check it's value
 // good for handling uncontrolled components -- useRef is not going to reRender so we always have to reach out to get the value
+
+// also possilbe to manage forms using state, when we use ref we create a uncontrolled component
+// 
 function App() {
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
   const txtTitle = useRef(),
     hexColor = useRef();
   const submit = e => {
 
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
+    const title1 = txtTitle.current.value;
+    const color1 = hexColor.current.value;
     alert(`${title}, ${color}`);
     txtTitle.current.value = "";
     hexColor.current.value = "";
@@ -21,8 +27,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <form onSubmit={submit}>
-          <input ref={txtTitle} type="text" placeholder='color title..' />
-          <input ref={hexColor} type="color" />
+          <input value={title} onChange={(event) => setTitle(event.target.value)} ref={txtTitle} type="text" placeholder='color title..' />
+          <input value={color} onChange={(event) => { setColor(event.target.value) }} ref={hexColor} type="color" />
           <button>ADD</button>
         </form>
       </header>
