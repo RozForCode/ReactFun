75e64239c7a798f3390de70406371946bb58f044
Navrose <navrosesinghjohal@gmail.com>
2024-08-03 20:22:32 -0400

    display the fetched data properly

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react'
 
+function GithubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{location}</h2>
+      <img src={avatar} alt={name} height='200px' width='200px' />
+    </div>
+  )
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -10,7 +20,7 @@ function App() {
       .then(data => setData(data));//shorthand could be .then(setData)
 
   }, []);
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>
+  if (data) return <GithubUser name={data.name} location={data.location} avatar={data.avatar_url} />
   return (
     <h1></h1>
   );
