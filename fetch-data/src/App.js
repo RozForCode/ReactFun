33699b9f7df54f8aaded159a03cecc15dc049f50
Navrose <navrosesinghjohal@gmail.com>
2024-08-04 11:20:14 -0400

    added comments to explain working of code and added code to handle all types of states

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -14,10 +14,15 @@ function GithubUser({ name, location, avatar }) {
 
 function App() {
   const [data, setData] = useState(null);
+  // handling loading states - loading state, success state and error state
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
   useEffect(() => {
     fetch(`https://api.github.com/users/RozForCode`)
       .then(res => res.json())
-      .then(data => setData(data));//shorthand could be .then(setData)
+      // data1 refers to the data that we get from the promise returned by previous .then()
+      // which is then used to update the value of useState data that has been set to null
+      .then(data1 => setData(data1));//shorthand could be .then(setData)
 
   }, []);
   if (data) return <GithubUser name={data.name} location={data.location} avatar={data.avatar_url} />
