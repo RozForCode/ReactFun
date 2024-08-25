9cf0786307a27b45faa93d240ed8382a658a285c
Navrose <navrosesinghjohal@gmail.com>
2024-08-25 00:16:11 -0400

    getting error in using tailwind css

--- react_interface/src/App.js
+++ react_interface/src/App.js
@@ -2,9 +2,9 @@ import { BiArchive } from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto mt-3 font-thin"  >
       <h1>NSJ</h1>
-      <BiArchive></BiArchive>
+      <BiArchive className="inline-block text-red-500"></BiArchive>
     </div>
   );
 }
